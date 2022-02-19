fb729a10e2143cee07c384f9bd9ef24c892c9648
rj <69528706+rj-riddhi@users.noreply.github.com>
2022-02-20 03:53:55 +0530

    Update Database.php

--- Helperland/models/Database.php
+++ Helperland/models/Database.php
@@ -10,28 +10,11 @@
 
 	 function __construct(){
 	 	$this->conn = $this->connectDB();
-	 	if($this->conn)
-	 	{
-	 		?>
-	 		<script>
-	 			alert("Database Connected");
-	 		</script>
-	 		<?php
-	 	}
-	 	else
-	 	{
-	 		?>
-	 		<script>
-	 			alert("Not connected");
-	 		</script>
-	 		<?php
-	 	}
-
 	 }
 
 	 function connectDB(){
 
-	 	$conn = mysqli_connect($this->host,$this->username,$this->pass,$this->db);
+	 	$conn = mysqli_connect($this->host,$this->username,$this->pass,$this->db) or die("Not connected");
 	 	return $conn;
 	 }
 
@@ -130,6 +113,111 @@
     	{
     		return flase;
     	}
+    }
+
+    public function avilablepostalcode($postalcode){
+    	
+    	$select_query = "SELECT * FROM zipcode WHERE ZipcodeValue = $postalcode";
+		$result = mysqli_query($this->conn , $select_query);
+		// $array = mysqli_fetch_assoc($result);
+		 $row = mysqli_num_rows($result);
+		 return $row;
+
+    }
+    public function City($pincode)
+    {
+
+        $select_query  = " SELECT
+        zipcode.ZipcodeValue,
+        city.CityName, state.StateName  FROM zipcode 
+      JOIN city
+        ON zipcode.CityId = city.Id  AND ZipcodeValue = $pincode
+		JOIN state 
+        ON state.Id = city.StateId";
+        $result = mysqli_query($this->conn,$select_query);
+        $array = mysqli_fetch_assoc($result);
+		$zipcode = $array['ZipcodeValue'];
+        $city = $array['CityName'];
+        $state = $array['StateName'];
+		return array($city, $state);
+    }
+    public function saveaddress($service_data)
+    {
+    	$userid = $service_data['userid'];
+    	$addresslinee1 = $service_data['street'];
+    	$addresslinee2 = $service_data['housenumber'];
+    	$city = $service_data['city'];
+    	$state = $service_data['state'];
+    	$postalcode = $service_data['postalcode'];
+    	$mobile = $service_data['phone'];
+    	$email = $service_data['email'];
+
+    	$insert_query = "INSERT INTO useraddress (UserId , AddressLine1	 , AddressLine2 , City ,State,  PostalCode , Mobile , Email)
+        VALUES ($userid,'$addresslinee1','$addresslinee2','$city','$state','$postalcode','$mobile','$email')";
+        $result = mysqli_query($this->conn,$insert_query);
+       
+        return $result;
+    }
+    public function getAddresses($result){
+    	$select_query = "SELECT * FROM useraddress WHERE PostalCode = '$result'";
+    	$result = mysqli_query($this->conn,$select_query);
+    	$array = mysqli_fetch_assoc($result);
+    	$row = mysqli_num_rows($result);
+    	return array($row, $array);
+    }
+
+    public function favourite_sp($userid){
+    	$select_query = "SELECT * FROM favoriteandblocked WHERE UserId = '$userid'";
+    	$result = mysqli_query($this->conn,$select_query);
+    	$array = mysqli_fetch_assoc($result);
+    	$row = mysqli_num_rows($result);
+    	return array($row,$array);
+    }
+    public function getuserid($email){
+    	$select_query = "SELECT * FROM user where Email = '$email' ";
+    	$result = mysqli_query($this->conn,$select_query);
+    	$array = mysqli_fetch_assoc($result);
+    	return $array;
+    }
+    public function GetUser($targetuserid){
+    	$select_query = "SELECT * FROM user where UserId = '$targetuserid' ";
+    	$result = mysqli_query($this->conn,$select_query);
+    	$array = mysqli_fetch_assoc($result);
+    	return $array;
+    }
+
+    public function Serviceadd($data){
+        $userid = $data['userid'];
+        $date = $data['date'];
+        $time = $data['time'];
+        $postalcode = $data['postalcode'] ;
+        $servicehourate = $data['servicehourate'] ;
+        $hrs = $data['hrs'];
+        $extrahrs = $data['extrahrs'];
+        $totalhrs = $data['totalhrs'];
+        $bed = $data['bed'];
+        $bath = $data['bath'] ;
+        $subtotal = $data['subtotal'];
+        $discount = $data['discount'];
+        $totalcost = $data['totalcost'];
+        $effectivecost = $data['effectivecost'];
+        $extraservices = $data['extraservices'];
+        $comment = $data['comment'];
+        $addid = $data['addid'];
+        $paymentrefno = $data['paymentrefno'];
+        $paymentdue = $data['paymentdue'];
+        $pets = $data['pets'];
+        $creationdate = date('d-m-Y');
+        $paymentdone = $data['paymentdone'];
+        $recordversion = $data['recordversion'];
+
+        $insert_query = "INSERT INTO servicerequest ( `UserId`, `ServiceStartDate`, `ServiceTime`, `ZipCode`,  `ServiceHourlyRate`, `ServiceHours`, `ExtraHours`, `TotalHours`, `TotalBed`, `TotalBath`, `SubTotal`, `Discount`, `TotalCost`, `EffectiveCost`, `ExtraServices`, `Comments`, `AddressId`, `PaymentTransactionRefNo`, `PaymentDue`, `HasPets`, `Status`, `CreatedDate`,  `PaymentDone`, `RecordVersion`)
+     VALUES ($userid,$date,$time,$postalcode,$servicehourate,$hrs,$extrahrs,$totalhrs,$bed,$bath,$subtotal,$discount,$totalcost,$effectivecost,$extraservices,$comment,$addid,$paymentrefno,$paymentdue,$pets,$creationdate,$paymentdone,$recordversion)";
+
+        $result = mysqli_query($this->conn,$insert_query);
+        $array = mysqli_fetch_assoc($result);
+        return $array['ServiceRequestId'];
+
     }
 
 
