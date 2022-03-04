9d20d1ad53e1505493d7e84f983008c9c73d578b
rj <69528706+rj-riddhi@users.noreply.github.com>
2022-03-05 00:23:51 +0530

    Update jsLinks.php

--- Helperland/views/jsLinks.php
+++ Helperland/views/jsLinks.php
@@ -1,9 +1,10 @@
 
  
+
+
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-
+<script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
 <!-- Admin_service_request's links -->
 <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
@@ -20,10 +21,15 @@
 
 
 
+
+
+
+
 <?php 
 	echo '<script src="../views//JavaScripts/Cookie_and_Scroll.js" type="text/javascript" charset="utf-8" async defer></script>';
-	echo '<script src="JavaScripts/admin_service_request.js"></script>';
-	echo '<script src="JavaScripts/export_data.js" type="text/javascript" charset="utf-8" async defer></script>';
+	// echo '<script src="JavaScripts/export_data.js" type="text/javascript" charset="utf-8" async defer></script>';
+	// echo '<script src="JavaScripts/admin_service_request.js"></script>';
+	
 	 ?>
 
 
